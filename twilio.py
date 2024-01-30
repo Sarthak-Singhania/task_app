16121d3adc60ef708b57ea9b8f8eea5babc84cf9
Sarthak-Singhania <sarthak.singhania28@gmail.com>
2024-01-30 23:03:00 +0530

    updated twilio to get ip automatically

--- twilio.py
+++ twilio.py
@@ -1,11 +1,19 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
 from twilio.rest import Client
 
-account_sid = "AC9e5a1a4a57fbef36669aaf08e4ecdd21"
-auth_token = "3e4d1fada98f366b044b6ca89ea68333"
+load_dotenv()
+
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
 
 def make_call(to, title, id, due_date):
+    ip = requests.get('http://checkip.amazonaws.com').text.strip()
     call = client.calls.create(
         to=f"+91{to}",
         from_="+14422765082",
@@ -16,7 +24,7 @@ def make_call(to, title, id, due_date):
                 <Say>Thank you</Say>
                 <Hangup/>
             </Response>''',
-        status_callback="http://54.167.176.24:5000/call_status"
+        status_callback=f"http://{ip}:5000/call_status"
     )
 
     return call.sid
